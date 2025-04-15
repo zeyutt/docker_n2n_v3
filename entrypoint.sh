a801e4adcd01d216ff463364b9c11fa80f3d504c
zyxai <86274360+zeyutt@users.noreply.github.com>
2025-04-15 17:07:10 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo type : ${type}
+echo 14
+mkdir -p /dev/net
+mknod /dev/net/tun c 10 200
+chmod 666 /dev/net/tun
+cat /dev/net/tun
+ls -l /dev/net/tun
+
+echo supernode: $supernodenet
+echo interfaceaddress: $interfaceaddress
+echo tun : $(lsmod | grep tun)
+if [[ ${type} == "supernode" ]]; then
+        /usr/local/sbin/supernode --help
+        /usr/local/sbin/supernode -l $listenport -f $OPTIONS
+elif [[ ${type} == "edge" ]]; then
+        # /usr/local/sbin/edge --help
+        /usr/local/sbin/edge -d $devicename -a $interfaceaddress -c $communityname -k $Encryptionkey -l $supernodenet -f $OPTIONS  
+fi
