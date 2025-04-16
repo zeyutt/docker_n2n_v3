08a1c9f9f68fa48e144b5ad0a69df6531a6aaa39
zyxai <86274360+zeyutt@users.noreply.github.com>
2025-04-16 11:50:27 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ echo interfaceaddress: $interfaceaddress
 echo tun : $(lsmod | grep tun)
 if [[ ${type} == "supernode" ]]; then
         /usr/local/sbin/supernode --help
-        /usr/local/sbin/supernode -l $listenport -f $OPTIONS
+        /usr/local/sbin/supernode -p $listenport -f $OPTIONS
 elif [[ ${type} == "edge" ]]; then
         # /usr/local/sbin/edge --help
         /usr/local/sbin/edge -d $devicename -a $interfaceaddress -c $communityname -k $Encryptionkey -l $supernodenet -f $OPTIONS  
